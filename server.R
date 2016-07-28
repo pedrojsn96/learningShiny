13ca0f7b7e5cff446b2741495973e7139dab1e7d
pedrojsn96 <pjsn@cin.ufpe.br>
2016-07-28 15:22:10 -0300

    Add library p/ deploy

--- server.R
+++ server.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(shinythemes)
+library(rsconnect)
 
 # Rely on the 'WorldPhones' dataset in the datasets
 # package (which generally comes preloaded).
@@ -11,23 +12,36 @@ shinyServer(function(input, output) {
   # Fill in the spot we created for a plot
   output$valueNum <- renderPrint({ input$numVar })
   
+  ##Para uma variavel
+  
   varInput <- reactive({
     return(input$variable)
   })
+  
+  ##Para duas variaveis
+  
+  varxInput <- reactive({
+    return(input$variableX)
+  })
+  varyInput <- reactive({
+    return(input$variableY)
+  })
+  
+  #---------------
   
   graficoPlot <- reactive({
     if(input$grafico == "barras"){
       # Render a barplot
       return(
         barplot(table(dados[,varInput()]), 
-              main=varInput(),
-              ylab="Frequencia",
-              xlab="Numeros"))
+                main=varInput(),
+                ylab="Frequencia",
+                xlab="Numeros"))
     }else{
       return(
         # Render a pie
         pie(table(dados[,varInput()]), 
-          main=varInput()))
+            main=varInput()))
     }
   })
   #output$texto <- renderText(graficoPlot())
@@ -43,5 +57,6 @@ shinyServer(function(input, output) {
     
     DT::datatable(as.data.frame(table(dados[,varInput()])))
   })
+  
   
 })
--- ui.R
+++ ui.R
@@ -1,53 +1,54 @@
-# Rely on the 'WorldPhones' dataset in the datasets
-# package (which generally comes preloaded).
-
 library(shinythemes)
+library(rsconnect)
 # Define the overall UI
 shinyUI(
   
   # Use a fluid Bootstrap layout
   fluidPage(theme = shinytheme("cerulean"),    
-    
-    # Give the page a title
-    titlePanel("App Shiny - Pedro Neto"),
-    
-    # Generate a row with a sidebar
-    sidebarLayout(      
-      
-      # Define the sidebar with one input
-      sidebarPanel(
-        numericInput("numVar", label = "Quantidade de Variaveis:", value = 1, min = 1,max = 2),
-        conditionalPanel("input.numVar == 1",
-                         selectInput("variable", "Variavel X:", 
-                                     choices=colnames(dados))
-                         ),
-        conditionalPanel("input.numVar == 2",
-                         selectInput("variableX", "Variavel X:", 
-                                     choices=colnames(dados)),
-                         selectInput("variableY", "Variavel Y:", 
-                                     choices=colnames(dados))
-                         ),
-        
-        selectInput("grafico", "Graficos:", 
-                    choices=c("Barras" = "barras","Pizza" = "pizza")),
-        hr(),
-        helpText("Dados de dados.csv")
-      ),
-      
-      # Create a spot for the barplot
-      mainPanel(
-        shinyUI(navbarPage("",
-                           tabPanel("Grafico",
-                                    plotOutput("varPlot")),
-                           tabPanel("Sumario",
-                                    verbatimTextOutput("sumario")),
-                           tabPanel("Tabela",
-                                    DT::dataTableOutput("tabela"))
-        ))  
-        #textOutput("texto")
-      )
-      
-      
-    ) 
+            
+            # Give the page a title
+            titlePanel("App Shiny - Pedro Neto"),
+            
+            # Generate a row with a sidebar
+            sidebarLayout(      
+              
+              # Define the sidebar with one input
+              sidebarPanel(
+                numericInput("numVar", label = "Quantidade de Variaveis:", value = 1, min = 1,max = 2),
+                conditionalPanel("input.numVar == 1",
+                                 selectInput("variable", "Variavel X:", 
+                                             choices=colnames(dados)),
+                                 selectInput("grafico", "Graficos:", 
+                                             choices=c("Barras" = "barras","Pizza" = "pizza"))
+                ),
+                conditionalPanel("input.numVar == 2",
+                                 selectInput("variableX", "Variavel X:", 
+                                             choices=colnames(dados)),
+                                 selectInput("variableY", "Variavel Y:", 
+                                             choices=colnames(dados)),
+                                 
+                                 selectInput("grafico", "Graficos:", 
+                                             choices=c("Barras" = "barrasx"))
+                ),
+                hr(),
+                helpText("Dados de dados.csv")
+              ),
+              
+              # Create a spot for the barplot
+              mainPanel(
+                shinyUI(
+                  navbarPage("",
+                             tabPanel("Grafico",
+                                      plotOutput("varPlot")),
+                             tabPanel("Sumario",
+                                      verbatimTextOutput("sumario")),
+                             tabPanel("Tabela",
+                                      DT::dataTableOutput("tabela"))
+                  ))  
+                #textOutput("texto")
+              )
+              
+              
+            ) 
   )
 )
